c178de42767f3e4accaf72e2a8d157e5c479da9e
bele27 <bbele12@yahoo.com>
2021-03-26 18:59:03 -0700

    netflix

---
+++ build/precache-manifest.d9c9fe473fdee9743e55e6f0eb127db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f75ff651dc4280aa06127bdaf37c04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53e3323404cccee00a9f",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e3323404cccee00a9f",
+    "url": "/static/js/main.b7d61b34.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
